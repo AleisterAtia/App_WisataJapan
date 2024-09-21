c55f986475218820af3145835bc39ad4c8f62e7b
CodifyArhab <fdzakyarhab@gmail.com>
2024-09-21 22:06:54 +0700

    first commit

---
+++ Latihan14AppWisataJepang/app/src/main/java/com/fadhiil2010/latihan_14_app_wisata_jepang/Model/WisataModel.kt
@@ -0,0 +1,11 @@
+package com.fadhiil2010.pertemuan03.model
+
+import android.hardware.biometrics.BiometricManager.Strings
+
+data class ModelMovie(
+    var title : String,
+    var image : Int,
+    val description: String
+
+
+)
